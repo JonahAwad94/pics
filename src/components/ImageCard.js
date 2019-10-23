99bc5aa96c7dbb7366dc83d2f3fc605f4fb60716
Jonah Alexander-Awad <34581478+JonahAwad94@users.noreply.github.com>
2019-10-23 19:41:36 -0400

    Completed Image Display Format

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,35 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { spans: 0 };
+
+    this.imageRef = React.createRef();
+  }
+
+  // once image is downloaded, get the height of the image
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
+
+  // Needed to set spans for grid-row-end property
+  // Arrow function so it's automatically bound
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 10);
+
+    this.setState({ spans: spans });
+  };
+
   render() {
     const { description, urls } = this.props.image;
 
     return (
-      <div>
-        <img alt={description} src={urls.regular} />
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
   }
